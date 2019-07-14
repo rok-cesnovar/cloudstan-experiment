f74c94d7d8c799dd4d12acbd4ef694936a2883c5
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-07-14 15:30:01 +0200

    removed github login due to errors when moving to heroku

--- server.js
+++ server.js
@@ -33,7 +33,31 @@ module.exports = app;
 fs.readdirSync(models)
   .filter(file => ~file.search(/^[^.].*\.js$/))
   .forEach(file => require(join(models, file)));
+/******
+ * 
+ * 
+ * 
+ * 
 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
 // Bootstrap routes
 require('./config/passport')(passport);
 require('./config/express')(app, passport);
